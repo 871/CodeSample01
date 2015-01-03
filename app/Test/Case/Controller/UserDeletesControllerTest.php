be63b9ece65d90cbe3fed73de6db518d640655eb
hanai <hanai@192.168.24.52>
2015-01-03 21:46:06 +0900

    アカウント削除

---
+++ app/Test/Case/Controller/UserDeletesControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('UserDeletesController', 'Controller');
+
+/**
+ * UserDeletesController Test Case
+ *
+ */
+class UserDeletesControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.user_delete'
+	);
+
+}
